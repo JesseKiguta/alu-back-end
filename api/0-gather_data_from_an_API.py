856543b6577a75fb3a0edce3ec27093f693833ee
JesseKiguta <j.kiguta@alustudent.com>
2023-11-02 00:47:23 +0200

    task zero

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,10 +1,31 @@
+#!/usr/bin/python3
+import requests
+import sys
+
 if __name__ == "__main__":
-    import requests
+    # Check for the correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-url = 'https://intranet.aluswe.com/rltoken/XNmscHBY0THdxQXM_MVzdw'
-response = requests.get(url)
+    # Get the employee ID from the command line argument
+    employee_id = sys.argv[1]
 
-data = response.json()
-name = data['EMPLOYEE_NAME']
-tasknum = data['NUMBER_OF_DONE_TASKS']
-tasksum = data['TOTAL_NUMBER_OF_TASKS']
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+    todo_response = requests.get(todo_url)
+    user_response = requests.get(user_url)
+
+    todo_data = todo_response.json()
+    user_data = user_response.json()
+
+    if 'name' in user_data:
+        name = user_data['name']
+
+    completed_tasks = [task for task in todo_data if task['userId'] == int(employee_id) and task['completed']]
+    total_tasks = [task for task in todo_data if task['userId'] == int(employee_id)]
+
+    print(f"Employee {name} is done with tasks({len(completed_tasks)}/{len(total_tasks)}):")
+    for task in completed_tasks:
+        print("\t " + task['title'])
