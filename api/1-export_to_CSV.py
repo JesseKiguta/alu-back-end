329e779588d32ecc2fa3df2ca3657b369b5a86fb
JesseKiguta <j.kiguta@alustudent.com>
2023-11-04 18:44:23 +0000

    :(

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-
 """
 Retrieve and print information about an employee's tasks.
 
 Args:
-employee_id (int): The employee ID.
+    employee_id (int): The employee ID.
 
 Prints:
-Employee progress information for each task in the following format:
-'USERID','USERNAME','TASK_COMPLETED_STATUS','TASK_TITLE'
+    Employee progress information for each task in the following format:
+    'USERID','USERNAME','TASK_COMPLETED_STATUS','TASK_TITLE'
 """
 
 import csv
@@ -17,14 +16,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-
     """
     Gets the employee ID
-
     Puts the links of APIs into variables
-
     Retrieves data from the API and parses the data
-
     Exports the data in the CSV format
     """
 
@@ -49,26 +44,25 @@ if __name__ == "__main__":
 
     # Open the CSV file for writing
     with open(csv_filename, 'w', newline='') as csv_file:
-	# Create a CSV writer object
+        # Create a CSV writer object
         csv_writer = csv.writer(csv_file)
-	# Write the CSV header
-    	csv_writer.writerow(["USER_ID", "USERNAME",
-			"TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        # Write the CSV header
+        csv_writer.writerow(["USER_ID", "USERNAME",
+                            "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
     # Iterate through the todo items
     for todo in todo_data:
-	# Gets the employees data to be printed
-	if todo.get('userId') == employee_id:
-		# Matches the user to their employee ID
-		user = next((user for user in user_data if
-			user['id'] == employee_id), None)
-                if user:
-		    # Writes task details to the CSV file
-                    employee_name = user['name']
-                    task_completed = "completed" if todo['completed']
-					else "not completed"
-                    task_title = todo['title']
-                    csv_writer.writerow([employee_id, employee_name,
-					task_completed, task_title])
+        # Gets the employees' data to be printed
+        if todo.get('userId') == employee_id:
+            # Matches the user to their employee ID
+            user = next((user for user in user_data if
+                        user['id'] == employee_id), None)
+            if user:
+                # Writes task details to the CSV file
+                employee_name = user['name']
+                task_completed = "completed" if todo['completed'] else "not completed"
+                task_title = todo['title']
+                csv_writer.writerow([employee_id, employee_name,
+                                    task_completed, task_title])
     # Confirm data is saved
     print(f"Data saved to {csv_filename}")
